a280079929f40d274298d8bbf46b3aacb284456e
NelsonNaja <50958575+NelsonNaja@users.noreply.github.com>
2020-01-13 23:15:45 +0100

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = `mongodb+srv://vrconnection:KSg2WnnTjvpb7JgT@rvconnections0-w205o.mongodb.net/vrconnection_db`;
+const uri = `mongodb+srv://user:pass@url_bd/bd`;
 
 export default callback => {
   // connect to a database
